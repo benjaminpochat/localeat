1e3090b8286fc2e25e5e92948b910b53488720df
Benjamin POCHAT <benjamin@MacBookPro.home>
2020-05-10 01:09:23 +0200

    setup docker-compose config to build and run

--- core/src/main/java/com/localeat/core/config/CommandLineDatabaseConfig.java
+++ core/src/main/java/com/localeat/core/config/CommandLineDatabaseConfig.java
@@ -1,7 +1,6 @@
 package com.localeat.core.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,19 +9,25 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile({"prod", "dev"})
-public class DatabaseConfig {
+@Profile({"prod"})
+public class CommandLineDatabaseConfig {
 
-    @Autowired
-    private DataSourceProperties dataSourceProperties;
+    @Value("${datasource_url}")
+    private String url;
+
+    @Value("${datasource_username}")
+    private String userName;
+
+    @Value("${datasource_password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
-                .username(dataSourceProperties.getUsername())
-                .password(dataSourceProperties.getPassword())
-                .url(dataSourceProperties.getUrl())
+                .username(userName)
+                .password(password)
+                .url(url)
                 .build();
     }
 }
---
+++ core/src/main/java/com/localeat/core/config/PropertiesDatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.localeat.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile({"dev"})
+public class PropertiesDatabaseConfig {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder
+                .create()
+                .username(dataSourceProperties.getUsername())
+                .password(dataSourceProperties.getPassword())
+                .url(dataSourceProperties.getUrl())
+                .build();
+    }
+}
