bb55aa737b19a2e687daa14e0941e983020ddc74
Benjamin POCHAT <benjamin@MacBookPro.home>
2020-10-06 22:25:49 +0200

    enable order by customers #12 - step 6 : login page for customers (missin file)

---
+++ ui/src/app/commons/services/authentication.service.mock.ts
@@ -0,0 +1,27 @@
+import { Account } from '../models/account.model';
+import { Actor } from '../models/actor.model';
+import { Authentication } from '../models/authentication.model';
+import { AuthenticationService } from './authentication.service';
+
+export class AuthenticationServiceMock {
+
+  private getAuthentication(): Authentication {
+    const actor = new Actor();
+    actor.email = 'louis.lachenal@montblanc.fr';
+    actor.name = 'Lachenal';
+    actor.firstName = 'Louis';
+    actor.phoneNumber = '01 23 45 67 89';
+    const account = new Account();
+    account.id = 1;
+    account.username = 'louis.lachenal';
+    account.actor = actor;
+    const authentication = new Authentication();
+    authentication.authorities = ['CUSTOMER'];
+    authentication.account = account;
+    return authentication;
+  }
+
+  public mockAuthenticationServiceMock(authenticationService: AuthenticationService): void {
+    authenticationService.getAuthenticationFromCookie = () => this.getAuthentication();
+  }
+}
