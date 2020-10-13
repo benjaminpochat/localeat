c07d86bc2fa90226f4e5e2c7d09a5d7462087827
Benjamin POCHAT <benjamin@MacBookPro.home>
2020-10-13 13:21:18 +0200

    fix ui unit tests

--- ui/src/app/customer-area/components/order-list/order-list.component.spec.ts
+++ ui/src/app/customer-area/components/order-list/order-list.component.spec.ts
@@ -1,14 +1,20 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Type } from '@angular/core';
 
 import { OrderListComponent } from './order-list.component';
+import { environment } from 'src/environments/environment';
+import { UrlService } from 'src/app/commons/services/url.service';
 
 describe('OrderListComponent', () => {
   let component: OrderListComponent;
   let fixture: ComponentFixture<OrderListComponent>;
+  let urlService: UrlService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderListComponent ]
+      declarations: [ OrderListComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   }));
@@ -16,6 +22,8 @@ describe('OrderListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OrderListComponent);
     component = fixture.componentInstance;
+    urlService = fixture.debugElement.injector.get<UrlService>(UrlService as Type<UrlService>);
+    spyOn(urlService, 'getAuthenticatedUrl').and.returnValue(environment.localeatCoreUrl + '/accounts/1/orders');
     fixture.detectChanges();
   });
 
