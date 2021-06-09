f469f662ac0c65e31eebd707ee9ae16f13c8e967
Benjamin POCHAT <benjamin.pochat@gmail.com>
2021-06-09 23:24:52 +0200

    fix ui tests errors

--- ui/src/app/customer-area/components/delivery/delivery.component.ts
+++ ui/src/app/customer-area/components/delivery/delivery.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { Delivery } from 'src/app/commons/models/delivery.model';
 import { DeliveryService } from 'src/app/customer-area/services/delivery.service';
 import { MatDialog } from '@angular/material/dialog';
-import { Slaughter } from 'src/app/commons/models/slaughter.model';
 import { Animal } from 'src/app/commons/models/animal.model';
-import { AnimalType } from 'src/app/commons/models/animal-type.model';
-import { AnimalBreed } from 'src/app/commons/models/animal-breed.model';
 import { AnimalService } from 'src/app/commons/services/animal.service';
 
 @Component({
